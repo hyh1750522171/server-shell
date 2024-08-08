8bc4a4248d54cb60386c23342745d94ffd78a2e6
mingjun <2667004988@qq.com>
2024-08-08 13:44:58 +0000

    修复bug

--- install_mb.sh
+++ install_mb.sh
@@ -9,8 +9,8 @@ else
     # 在此处添加存在时要执行的命令
     echo "检测到没有安装 1panel 面板，建议您先安装 1panel 面板"
     # 提示用户是否安装
-    read -p "是否安装 1panel 面板？ (yes/no): " answer
-    if [[ "$answer" =~ ^[Yy][Ee][Ss]$ ]]; then
+    read -p "是否安装 1panel 面板？ (yes/no): " shuru
+    if [[ "$shuru" =~ ^[Yy][Ee][Ss]$ ]]; then
         # 下载安装脚本
         install_1panel
         echo "1panel 面板已安装完成."
